105aec8238480b5af8afd33ca3e4d682b62f71ce
胡孝飞 <hxfsc2@gmail.com>
2021-04-07 23:19:51 +0800

    feat: Icon

--- lib/main.dart
+++ lib/main.dart
@@ -28,53 +28,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  final wordPair = new WordPair.random();
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new RandomWords(),
-      ),
-
-    );
-  }
-}
-
-
 class RandomWords extends StatefulWidget{
   @override
   createState() => new RandomWordsState();
@@ -95,6 +48,7 @@ class RandomWordsState extends State<RandomWords>{
 
   final _suggestions = <WordPair>[];
   final _biggerFront = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
 
   Widget _buildSuggestions(){
     return new ListView.builder(
@@ -113,8 +67,23 @@ class RandomWordsState extends State<RandomWords>{
   }
 
   Widget _buildRow(WordPair pair){
+
+    final alreadSaved = _saved.contains(pair);
     return ListTile(
-      title: new Text(pair.asCamelCase, style: _biggerFront)
+      title: new Text(pair.asCamelCase, style: _biggerFront),
+      trailing: new Icon(
+        alreadSaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadSaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadSaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
